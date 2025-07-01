60bf6473d7783d38a0c20593bf40153cefd0e279
Hoàng Lê Thành Đức <fptuduchoang@gmail.com>
2025-07-01 11:38:31 +0700

    chapter 8 updated

---
+++ MyStoreWpfApp_EF/AdminWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using MyStoreWpfApp_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWpfApp_EF
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        MyStoreContext context = new MyStoreContext();
+        public AdminWindow()
+        {
+            InitializeComponent();
+            LoadCategoriresIntoTreeView();
+        }
+
+        private void LoadCategoriresIntoTreeView()
+        {
+            //Tạo gốc cây
+            tvCategory.Items.Clear();
+            TreeViewItem root = new TreeViewItem();
+            root.Header = "Danh mục sản phẩm";
+            tvCategory.Items.Add(root);
+            //Dùng EF truy cấn toàn bộ danh mục và nạp lên Trê:
+            var categories = context.Categories.ToList();
+            foreach ( var category in categories )
+            {
+                //tạo node cho danh mục
+                TreeViewItem cate_node = new TreeViewItem();
+                cate_node.Header = category.CategoryName;
+                cate_node.Tag = category;
+                root.Items.Add(cate_node);
+
+                //nạp danh sách Sản phẩm vào node danh mục:
+                var products = context.Products
+                    .Where(x=>x.CategoryId == category.CategoryId)
+                    .ToList();
+                foreach (var product in products)
+                {
+                    //Tao product node
+                    TreeViewItem product_node = new TreeViewItem();
+                    product_node.Header = product.ProductName;
+                    product_node.Tag = product;
+                    cate_node.Items.Add(product_node);
+                }
+            }
+            root.ExpandSubtree();   
+        }
+
+        private void tvCategory_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue == null)
+                return;
+            TreeViewItem item = e.NewValue as TreeViewItem;
+            Category category = item.Tag as Category;
+            if(category == null) 
+                return;
+            LoadProductIntoListView(category);
+        }
+
+        private void LoadProductIntoListView(Category category)
+        {
+            var products = context.Products
+                    .Where(x => x.CategoryId == category.CategoryId)
+                    .ToList();
+            lvProduct.ItemsSource = null;
+            lvProduct.ItemsSource = products;
+        }
+    }
+}
---
+++ MyStoreWpfApp_EF/LoginWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using MyStoreWpfApp_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWpfApp_EF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        MyStoreContext context = new MyStoreContext();
+        public LoginWindow()
+        {
+            InitializeComponent();
+            ChangeBackground(); 
+        }
+
+        private void ChangeBackground()
+        {
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.GradientStops.Add(new GradientStop(Colors.PaleVioletRed, 0.0));
+            brush.GradientStops.Add(new GradientStop(Colors.Purple, 1.0));
+            brush.GradientStops.Add(new GradientStop(Colors.Yellow, 0.5));
+            btnThoat.Background = brush;
+
+            RadialGradientBrush gradientBrush = new RadialGradientBrush();
+            gradientBrush.GradientOrigin = new Point(0.25, 0.75); //góc xoay
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.RoyalBlue, 0.0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Tomato, 0.5));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Violet, 1.0));
+            btnDangNhap.Background = gradientBrush;
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+            AccountMember am = context.AccountMembers
+                .FirstOrDefault(a => a.EmailAddress == email && a.MemberPassword == password);
+            if(am == null)
+            {
+                MessageBox.Show("Đăng nhập thất bại", "Login failed", 
+                    MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                return;
+            }
+            else
+            {
+                if (am.MemberRole == 1)
+                {
+                    MessageBox.Show("Đăng nhập ADMIN thành công", "ADMIN Login successfully", 
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    AdminWindow aw = new AdminWindow();
+                    aw.Show();
+                    Close();
+                }
+                else if(am.MemberRole == 2)
+                {
+                    MessageBox.Show("Đăng nhập STAFF thành công", "STAFF Login successfully", 
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (am.MemberRole == 3)
+                {
+                    MessageBox.Show("Đăng nhập MEMBER thành công", "MEMBER Login successfully", 
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+    }
+}
